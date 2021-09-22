af9ed2aa3acd8ab036d499c40dfbd2061f9ad1ba
PerairaCC <carloscoto0103@gmail.com>
2021-09-22 15:53:06 -0600

    Installation of provider and create class

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,27 @@
+import 'package:agent_management/providers/agent_provider.dart';
 import 'package:flutter/material.dart';
 
 import 'package:agent_management/routes/routes.dart';
+import 'package:provider/provider.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Material App',
-      debugShowCheckedModeBanner: false,
-      initialRoute: 'create',
-      routes: getRoutes(),
+    return MultiProvider(
+      providers: [
+
+        ChangeNotifierProvider(
+          create: ( _ ) => AgentManamegent()
+        )
+
+      ],
+      child: MaterialApp(
+        title: 'Material App',
+        debugShowCheckedModeBanner: false,
+        initialRoute: 'home',
+        routes: getRoutes(),
+      ),
     );
   }
 }
