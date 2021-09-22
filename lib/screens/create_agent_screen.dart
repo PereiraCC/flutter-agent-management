79d4cc67fe686909365fd73d7856cc542421610e
PerairaCC <carloscoto0103@gmail.com>
2021-09-21 21:43:28 -0600

    Creation of screens and routes

---
+++ lib/screens/create_agent_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+
+class CreateAgentScreen extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(  
+        title: Text('Create Agent Screen'),
+      ),
+      body: Center(
+        child: Text('Create Agent Screen'),
+     ),
+   );
+  }
+}
---
+++ lib/screens/search_agent_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+
+class SearchAgentScreen extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(  
+        title: Text('Search Agent Screen'),
+      ),
+      body: Center(
+        child: Text('Search Agent Screen'),
+     ),
+   );
+  }
+}
