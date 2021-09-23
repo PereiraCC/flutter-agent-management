77f66c55b59cc9e271ce403ba9648d6b41ec6bec
PerairaCC <carloscoto0103@gmail.com>
2021-09-23 11:03:48 -0600

    Loading alert added

---
+++ lib/helpers/loading_alert.dart
@@ -0,0 +1,24 @@
+
+
+import 'package:flutter/material.dart';
+
+void _onLoading(BuildContext context) {
+  showDialog(
+    context: context,
+    barrierDismissible: false,
+    builder: (BuildContext context) {
+      return Dialog(
+        child: new Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            new CircularProgressIndicator(),
+            new Text("Loading"),
+          ],
+        ),
+      );
+    },
+  );
+  new Future.delayed(new Duration(seconds: 3), () {
+    Navigator.pop(context); //pop dialog
+  });
+}
