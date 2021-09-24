5eeaf9f7e24f34538c0a73f972bc5ac24f7fe250
PerairaCC <carloscoto0103@gmail.com>
2021-09-23 20:51:45 -0600

    No agents card added and custom appbar

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,3 @@
-import 'package:agent_management/helpers/loading_alert.dart';
 import 'package:agent_management/models/agent.dart';
 import 'package:agent_management/providers/agent_provider.dart';
 import 'package:agent_management/services/agent_service.dart';
@@ -15,7 +14,7 @@ class HomeScreen extends StatelessWidget {
     final agentsProvider = Provider.of<AgentManamegentProvider>(context);
 
     return Scaffold(
-      appBar: AppBarCustom(),
+      appBar: AppBarCustom('Agents list'),
       body: (!agentsProvider.loading) 
               ? _CreateBody(agentsProvider: agentsProvider) 
               : Center(
@@ -53,9 +52,7 @@ class _CreateBody extends StatelessWidget {
 
         if(snapshot.data!.length == 0){
           // TODO: Container no data
-          return Center(
-            child: Text('No agents')
-          );
+          return _NoData();
         }
 
         final agents = snapshot.data;
@@ -68,6 +65,82 @@ class _CreateBody extends StatelessWidget {
           }
         );
       }
+    );
+  }
+}
+
+class _NoData extends StatelessWidget {
+  const _NoData({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        
+        Container(  
+          height: 200,
+          margin: EdgeInsets.only(top: 80, right: 10, left: 10),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.all(Radius.circular(15)),
+            color: Colors.white,
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey.withOpacity(0.5),
+                spreadRadius: 5,
+                blurRadius: 7,
+                offset: Offset(0, 5),
+              )
+            ]
+          ),
+        ),
+
+        Container(
+          margin: EdgeInsets.only(top: 40, left: 120),
+          child: ImageAgent(
+            wid: 100,
+            hei: 100,
+            urlImage: 'assets/male-icon.jpg',
+          )
+        ),
+
+        Container(
+          margin: EdgeInsets.only(top: 150),
+          // color: Colors.red,
+          width: 400,
+          child: Column(
+            children: [
+
+              TextCustom(  
+                text: 'No agents',
+                size: 20,
+                font: FontWeight.bold,
+                color: Colors.red.shade300,
+              ),
+
+              SizedBox(height: 10),
+
+              TextCustom(  
+                text: 'Please press the + button to add a new agent',
+                size: 15,
+                font: FontWeight.normal,
+                color: Colors.grey
+              ),
+
+              Divider(thickness: 1, height: 20, endIndent: 40, indent: 40),
+
+              TextCustom(  
+                text: 'Please press the sync button to get new agents',
+                size: 15,
+                font: FontWeight.normal,
+                color: Colors.grey
+              ),
+            ]
+          )
+        )
+
+      ] ,
     );
   }
 }
--- lib/widgets/appbar_custom.dart
+++ lib/widgets/appbar_custom.dart
@@ -2,20 +2,27 @@ part of 'widgets.dart';
 
 class AppBarCustom extends StatefulWidget implements PreferredSizeWidget {
 
-  AppBarCustom() : preferredSize = Size.fromHeight(kToolbarHeight);
+  final String title;
+
+  AppBarCustom(this.title) : preferredSize = Size.fromHeight(kToolbarHeight);
 
   @override
-  _AppBarCustomState createState() => _AppBarCustomState();
+  _AppBarCustomState createState() => _AppBarCustomState(this.title);
 
   @override
   final Size preferredSize;
 }
 
 class _AppBarCustomState extends State<StatefulWidget> {
+
+  final String title;
+
+  _AppBarCustomState(this.title);
+
   @override
   Widget build(BuildContext context) {
     return AppBar(  
-      title: Text('Create Agent', style: TextStyle(color: Colors.red.shade300)),
+      title: Text(this.title, style: TextStyle(color: Colors.red.shade300)),
       centerTitle: true,
       backgroundColor: Colors.white,
       elevation: 10.0,
