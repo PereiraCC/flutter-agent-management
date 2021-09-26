aff0130e4709fd5d3d99050a65a68c32f34618c6
PerairaCC <carloscoto0103@gmail.com>
2021-09-26 16:09:20 -0600

    Remove search screen

--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -3,14 +3,12 @@ import 'package:flutter/material.dart';
 
 import 'package:agent_management/screens/home_screen.dart';
 import 'package:agent_management/screens/create_agent_screen.dart';
-import 'package:agent_management/screens/search_agent_screen.dart';
 
 Map<String, WidgetBuilder> getRoutes() {
 
   return <String, WidgetBuilder> {
     'home'   : (_) => HomeScreen(),
     'create' : (_) => CreateAgentScreen(),
-    'search' : (_) => SearchAgentScreen(),
   };
 
 }
