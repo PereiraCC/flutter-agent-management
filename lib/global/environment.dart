9cbef2029397136cd0e1c31fd0b81aa1013e35b2
PerairaCC <carloscoto0103@gmail.com>
2021-10-02 19:45:46 -0600

    environment.dart: apiUploadAgentsUrl added

--- lib/global/environment.dart
+++ lib/global/environment.dart
@@ -3,5 +3,6 @@
 class Environment {
 
   static String apiAgentsUrl = 'https://rest-server-agents-pereira.herokuapp.com/api/agents';
+  static String apiUploadAgentsUrl = 'https://rest-server-agents-pereira.herokuapp.com/api/uploads/agents';
 
 }
