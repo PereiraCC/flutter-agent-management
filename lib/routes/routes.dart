837585494c2fae53be5bb9e822d79f97d9477eab
PerairaCC <carloscoto0103@gmail.com>
2021-10-13 10:58:44 -0600

    sing_screen.dart: added, add new route

--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -1,12 +1,14 @@
 
 import 'package:flutter/material.dart';
 
+import 'package:agent_management/screens/sing_screen.dart';
 import 'package:agent_management/screens/home_screen.dart';
 import 'package:agent_management/screens/create_agent_screen.dart';
 
 Map<String, WidgetBuilder> getRoutes() {
 
   return <String, WidgetBuilder> {
+    'sing'   : (_) => SingScreen(),
     'home'   : (_) => HomeScreen(),
     'create' : (_) => CreateAgentScreen(),
   };
