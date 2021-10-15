77ad2e105df2cf7b7ff4cdc2f5ed7b82679d4694
PerairaCC <carloscoto0103@gmail.com>
2021-10-15 09:57:46 -0600

    user.ts (model): fix name class

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -4,12 +4,12 @@
 
 import 'dart:convert';
 
-List<Agent> agentFromJson(String str) => List<Agent>.from(json.decode(str).map((x) => Agent.fromJson(x)));
+List<User> agentFromJson(String str) => List<User>.from(json.decode(str).map((x) => User.fromJson(x)));
 
-String agentToJson(List<Agent> data) => json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
+String agentToJson(List<User> data) => json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
 
-class Agent {
-    Agent({
+class User {
+    User({
         this.uid,
         this.profileImage,
         required this.google,
@@ -25,7 +25,7 @@ class Agent {
     String identification;
     String name;
 
-    factory Agent.fromJson(Map<String, dynamic> json) => Agent(
+    factory User.fromJson(Map<String, dynamic> json) => User(
         uid: json["uid"],
         profileImage: json["profile_image"],
         google: json["google"],
