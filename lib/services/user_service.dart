d0e45ea8341797958304933a4f8f240d0b02d571
Carlos Pereira <carloscoto0103@gmail.com>
2021-10-16 20:14:52 -0600

    user_service.dart: login method added

--- lib/services/user_service.dart
+++ lib/services/user_service.dart
@@ -1,8 +1,11 @@
 import 'dart:convert';
+import 'package:agent_management/providers/user_provider.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:http/http.dart' as http;
 
 import 'package:agent_management/global/environment.dart';
 import 'package:agent_management/models/user.dart';
+import 'package:provider/provider.dart';
 
 class UserService { 
 
@@ -32,5 +35,49 @@ class UserService {
 
   }
 
+  static Future<bool> login(String email, String pass, BuildContext context) async {
 
+    try {
+      
+      final userProvider = Provider.of<UserProvider>(context, listen: false);
+
+      Uri url = Uri.parse('${Environment.apiAuthLoginUrl}');
+
+      final data = {
+        'email'    : email,
+        'password' : pass
+      };
+
+      final resp = await http.post(url, 
+        headers: {
+          'Content-Type' : 'application/json; charset=utf-8',
+        },
+        body: jsonEncode(data)
+      );
+      
+      final decodedData = json.decode(resp.body);
+
+      if (resp.statusCode == 200) {
+
+        userProvider.token = decodedData['token'];
+        userProvider.user = User.fromJson(decodedData['documents'][0]);
+        return true;
+
+      } else if(resp.statusCode == 400 || resp.statusCode == 404) {
+
+        userProvider.msgError = decodedData['msg'];
+        return false;
+
+      } else {
+
+        return false;
+
+      }
+
+    } catch (err) {
+      print('Error: $err');
+      return false;
+    }
+
+  }
 }
