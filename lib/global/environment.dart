3fcbdda709529260bc64c9eedc32e65f8ec127ff
Carlos Pereira <carloscoto0103@gmail.com>
2021-10-16 19:10:30 -0600

    enrironment.dart: apiAuthLoginUrl, apiAuthGoogleUrl paths added

--- lib/global/environment.dart
+++ lib/global/environment.dart
@@ -2,8 +2,12 @@
 
 class Environment {
 
+  static String apiAuthLoginUrl    = 'https://rest-server-agents-pereira.herokuapp.com/api/auth/login';
+  static String apiAuthGoogleUrl   = 'https://rest-server-agents-pereira.herokuapp.com/api/auth/google';
+
   static String apiAgentsUrl       = 'https://rest-server-agents-pereira.herokuapp.com/api/agents';
   static String apiUserUrl         = 'https://rest-server-agents-pereira.herokuapp.com/api/users';
+
   static String apiUploadAgentsUrl = 'https://rest-server-agents-pereira.herokuapp.com/api/uploads/agents';
   static String apiUploadUsersUrl  = 'https://rest-server-agents-pereira.herokuapp.com/api/uploads/users';
 
