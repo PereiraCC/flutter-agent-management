a20bfc5a8c15ce25fb74743d43827ec6e7c7db1d
Carlos Pereira <carloscoto0103@gmail.com>
2021-11-06 20:13:02 -0600

    home_screen.dart: design ready in home screen and images added

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -9,20 +9,23 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBarCustom('Management', Tabs.Home),
       body: Container(
-        child: Column(  
-
-          children: [
-
-            // Header
-            _Header()
-
-          ],
-
+        child: SingleChildScrollView(
+          physics: BouncingScrollPhysics(),
+          child: Column(  
+            children: [
+        
+              _Header(),
+        
+              _Body(),
+        
+            ],
+          ),
         )
      ),
    );
   }
 }
+
 
 class _Header extends StatelessWidget {
   const _Header({
@@ -33,7 +36,6 @@ class _Header extends StatelessWidget {
   Widget build(BuildContext context) {
 
     return Container(
-      // color: Colors.red,
       height: 150,
       margin: EdgeInsets.all(10),
       child: Row(  
@@ -46,7 +48,6 @@ class _Header extends StatelessWidget {
               children: [
 
                 Container(
-                  // color: Colors.green,
                   height: 100,
                   width: 250,
                   margin: EdgeInsets.all(10),
@@ -118,5 +119,180 @@ class _Header extends StatelessWidget {
       return 'Good Night';
     }
 
+  }
+}
+
+class _Body extends StatelessWidget {
+  const _Body({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 500,
+      margin: EdgeInsets.all(10),
+      child: Column(  
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+
+          // Message
+          Container(
+            margin: EdgeInsets.only(left: 10),
+            child: Text(
+              'My Company Management', 
+              style: TextStyle(
+                color: Colors.grey,
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+
+          SizedBox(height: 25),
+        
+          // Widget Card
+          _CategoryCard(
+            color: Colors.blue.shade50,
+            urlImage: 'assets/agents.jpg',
+            title: 'Agents',
+            subtitle: 'Total 6 Agents',
+            text: 'Management your agents',
+            event: () => Navigator.pushNamed(context, 'agent'),
+          ),
+
+          SizedBox(height: 20),
+
+          _CategoryCard(
+            color: Colors.purple.shade50,
+            urlImage: 'assets/products.jpg',
+            title: 'Products',
+            subtitle: 'Total 5 Products',
+            text: 'Management your products',
+            event: () => Navigator.pushNamed(context, 'agent'),
+          ),
+
+          SizedBox(height: 20),
+
+          _CategoryCard(
+            color: Colors.orange.shade50,
+            urlImage: 'assets/users.jpg',
+            title: 'Profile',
+            subtitle: 'Carlos Pereira',
+            text: 'Management your account',
+            event: () => Navigator.pushNamed(context, 'agent'),
+          ),
+
+          SizedBox(height: 20),
+
+        ],
+      ),
+
+    );
+  }
+}
+
+class _CategoryCard extends StatelessWidget {
+
+  final Color color;
+  final String urlImage;
+  final String title;
+  final String subtitle;
+  final String text;
+  final Function event;
+
+  const _CategoryCard({
+    Key? key, 
+    required this.color, 
+    required this.urlImage, 
+    required this.title, 
+    required this.subtitle, 
+    required this.text, 
+    required this.event,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      child: Container(
+        margin: EdgeInsets.only(left: 10),
+        height: 175,
+        width: 375,
+        decoration: BoxDecoration(  
+          color: this.color,
+          borderRadius: BorderRadius.all(Radius.circular(20))
+        ),
+        child: Row(  
+          children: [
+    
+            // Image
+            Container(
+              height: 80,
+              width: 100,
+              // color: Colors.green,
+              margin: EdgeInsets.only(left: 20, bottom: 70),
+              child: ImageAgent(
+                hei: 70, 
+                wid: 70, 
+                urlImage: this.urlImage
+              ),
+            ),
+    
+            // Text
+            Container(
+              width: 240,
+              height: 150,
+              // color: Colors.green,
+              margin: EdgeInsets.only(top: 10, bottom: 20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,  
+                children: [
+    
+                  Container(
+                    margin: EdgeInsets.all(10),
+                    child: Text(
+                      this.title,
+                      style: TextStyle( 
+                        fontSize: 25,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+    
+                  Container(
+                    margin: EdgeInsets.all(10),
+                    child: Text(
+                      this.subtitle,
+                      style: TextStyle( 
+                        fontSize: 15,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.grey,
+                      ),
+                    ),
+                  ),
+    
+                  SizedBox(height: 10),
+    
+                  Container(
+                    margin: EdgeInsets.all(10),
+                    // color: Colors.green,
+                    child: Text(
+                      this.text,
+                      style: TextStyle( 
+                        fontSize: 17,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.grey,
+                      ),
+                    ),
+                  ),
+    
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+      onTap: () { this.event(); }
+    );
   }
 }
