60379aecf400876804a6378ddc6ac9babf8da763
Carlos Pereira <carloscoto0103@gmail.com>
2021-11-13 12:23:45 -0600

    enviroment.dart: products path added

--- lib/global/environment.dart
+++ lib/global/environment.dart
@@ -8,6 +8,7 @@ class Environment {
 
   static String apiAgentsUrl       = 'https://rest-server-agents-pereira.herokuapp.com/api/agents';
   static String apiUserUrl         = 'https://rest-server-agents-pereira.herokuapp.com/api/users';
+  static String apiProductsUrl     = 'https://rest-server-agents-pereira.herokuapp.com/api/products';
 
   static String apiUploadAgentsUrl = 'https://rest-server-agents-pereira.herokuapp.com/api/uploads/agents';
   static String apiUploadUsersUrl  = 'https://rest-server-agents-pereira.herokuapp.com/api/uploads/users';
