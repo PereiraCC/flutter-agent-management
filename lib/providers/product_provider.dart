6f0933efb3ede3d3cfbeb5dcad1726f129bf14bc
Carlos Pereira <carloscoto0103@gmail.com>
2021-11-13 17:31:24 -0600

    product_provider.dart: isLoading property added

--- lib/providers/product_provider.dart
+++ lib/providers/product_provider.dart
@@ -5,8 +5,15 @@ import 'package:flutter/material.dart';
 
 class ProductProvider with ChangeNotifier {
 
+  bool _isLoading = false;
   List<Product> _products  = [];
   Product _product = new Product.empty();
+
+  bool get isLoading => _isLoading;
+  set isLoading(bool data){
+    this._isLoading = data;
+    notifyListeners();
+  }
 
   List<Product> get products => _products;
   set products(List<Product> data){
