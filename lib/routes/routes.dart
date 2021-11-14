81a0d9167a8e5b508d5a53d5d36c6b38874fa6d1
Carlos Pereira <carloscoto0103@gmail.com>
2021-11-14 12:55:41 -0600

    routes.dart: ProductsOptions routes added

--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -1,25 +1,24 @@
 
-import 'package:agent_management/screens/home_screen.dart';
 import 'package:flutter/material.dart';
 
 import 'package:agent_management/screens/screens.dart';
 
-
 Map<String, WidgetBuilder> getRoutes() {
 
   return <String, WidgetBuilder> {
-    'checking'      : (_) => CheckAuthScreen(),
+    'checking'       : (_) => CheckAuthScreen(),
 
-    'home'          : (_) => HomeScreen(),
+    'home'           : (_) => HomeScreen(),
 
-    'sing'          : (_) => SingScreen(),
-    'login'         : (_) => LoginScreen(),
-    'editProfile'   : (_) => EditProfileScreen(),
+    'sing'           : (_) => SingScreen(),
+    'login'          : (_) => LoginScreen(),
+    'editProfile'    : (_) => EditProfileScreen(),
 
-    'agent'         : (_) => AgentScreen(),
-    'agentsOptions' : (_) => AgentsOptionsScreen(),    
+    'agent'          : (_) => AgentScreen(),
+    'agentsOptions'  : (_) => AgentsOptionsScreen(),    
 
-    'product'       : (_) => ProductsScreen(),
+    'product'        : (_) => ProductsScreen(),
+    'productOptions' : (_) => ProductsOptionsScreen(),
   };
 
 }
--- lib/screens/screens.dart
+++ lib/screens/screens.dart
@@ -1,9 +1,11 @@
 
+
 export 'package:agent_management/screens/agent_screen.dart';
 export 'package:agent_management/screens/agents_options_screen.dart';
 export 'package:agent_management/screens/check_auth_screen.dart';
 export 'package:agent_management/screens/edit_profile_screen.dart';
 export 'package:agent_management/screens/home_screen.dart';
 export 'package:agent_management/screens/login_screen.dart';
-export 'package:agent_management/screens/sing_screen.dart';
+export 'package:agent_management/screens/products_options_screen.dart';
 export 'package:agent_management/screens/products_screen.dart';
+export 'package:agent_management/screens/sing_screen.dart';
