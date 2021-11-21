724c2419279d8f435346af1bca039d51ff75c2cc
Carlos Pereira <carloscoto0103@gmail.com>
2021-11-20 20:02:03 -0600

    body_product.dart: Form added

--- lib/widgets/body_product.dart
+++ lib/widgets/body_product.dart
@@ -11,7 +11,7 @@ class BodyProduct extends StatelessWidget {
     return Stack(
       children: [
 
-        BoxBackgroundBody(height: 800),
+        BoxBackgroundBody(height: 650),
 
         _ProductCircleImage(),
 
@@ -26,7 +26,7 @@ class BodyProduct extends StatelessWidget {
                   : Container(),
         ),
 
-        // _Content(agentProvider: agentProvider)
+        _ContentProducts()
 
       ]
     );
@@ -38,23 +38,343 @@ class _ProductCircleImage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
 
-    final agentPro = Provider.of<AgentManamegentProvider>(context);
+    final productPro = Provider.of<ProductProvider>(context);
     final widthScreen = MediaQuery.of(context).size.width;
 
     return Container(
       margin: EdgeInsets.only(top: 140, left: widthScreen * 0.35),
-      child: (!agentPro.isChangePhoto) ? ImageAgent(
+      child: (!productPro.isChangePhoto) ? ImageAgent(
         wid: 100,
         hei: 100,
-        networkImage: (agentPro.updating) 
-                      ? (agentPro.agent.profileImage == 'no-image') ? false : true 
+        networkImage: (productPro.isUpdating) 
+                      ? (productPro.product.profileImage == 'no-image') ? false : true 
                       : false,
-        urlImage: (agentPro.updating) 
-                    ? (agentPro.agent.profileImage != 'no-image') 
-                          ? agentPro.agent.profileImage ?? 'assets/no-image.jpg'
+        urlImage: (productPro.isUpdating) 
+                    ? (productPro.product.profileImage != 'no-image') 
+                          ? productPro.product.profileImage ?? 'assets/no-image.jpg'
                           : 'assets/no-image.jpg'
                     : 'assets/no-image.jpg',
-      ) : UploadImage(photo: agentPro.photo)
+      ) : UploadImage(photo: productPro.photo)
     );
   }
+}
+
+class _ContentProducts extends StatelessWidget {
+
+  const _ContentProducts({Key? key}) : super(key: key);
+  
+  @override
+  Widget build(BuildContext context) {
+
+    final productProvider = Provider.of<ProductProvider>(context, listen: false);
+    
+    return Container(
+      margin: EdgeInsets.only(top: 250),
+      width: 400,
+      child: Column(
+        children: [
+
+          // Title
+          TextCustom( 
+            text: (productProvider.isUpdating) ? 'Update Product' :'New Product',
+            size: 20,
+            font: FontWeight.bold,
+            color: Colors.red.shade300,
+          ),
+
+          SizedBox(height: 10),
+
+          // SubTitle
+          TextCustom(
+            text: (productProvider.isUpdating) ? '${productProvider.product.title}' : 'Please complete the product information',
+            size: 15,
+            font: FontWeight.normal,
+            color: Colors.grey
+          ),
+
+          Divider(thickness: 1, height: 20, endIndent: 40, indent: 40),
+
+          SizedBox(height: 15),
+
+          _FormProducts(productProvider),
+        ],
+      ),
+    );
+  }
+}
+
+class _FormProducts extends StatelessWidget {
+
+  final ProductProvider productProvider;
+
+  const _FormProducts(this.productProvider);
+    
+  @override
+  Widget build(BuildContext context) {
+
+    final productPro = Provider.of<ProductProvider>(context);
+
+    // Create controller for eath input
+    final TextEditingController _codeController  = new TextEditingController();
+    final TextEditingController _titleController = new TextEditingController();
+    final TextEditingController _priceController = new TextEditingController();
+
+    return Padding(
+      padding: EdgeInsets.only(left: 20, right: 20),
+      child: Column(
+        children: [
+
+          InputTitleCustom(text: 'Code'),
+          SizedBox(height: 10),
+          CustomInput(
+            hintText: (productProvider.isUpdating ? productProvider.product.code : ''), 
+            helpText: 'Example: 105', 
+            icon: Icons.pin_rounded, 
+            controller: _codeController, 
+            enable: productProvider.isUpdating ? false : true
+          ),
+
+          SizedBox(height: 20),
+
+          InputTitleCustom(text: 'Title'),
+          SizedBox(height: 10),
+          CustomInput(
+            hintText: productProvider.isUpdating ? productProvider.product.title : '',
+            helpText: 'Example: Iphone X', 
+            icon: Icons.title_outlined, 
+            controller: _titleController
+          ),
+
+          SizedBox(height: 20),
+
+          InputTitleCustom(text: 'Price'),
+          SizedBox(height: 10),
+          CustomInput(
+            hintText: productProvider.isUpdating ? productProvider.product.price.toString() : '',
+            helpText: 'Example: 1000', 
+            icon: Icons.attach_money_outlined, 
+            controller: _priceController
+          ),
+
+          SizedBox(height: 20),
+
+          InputTitleCustom(text: 'Available'),
+          SizedBox(height: 10),
+          Container(
+            // color: Colors.red,
+            padding: EdgeInsets.only(left: 20, right: 20),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+
+                Container(
+                  // color: Colors.red,
+                  child: Switch(
+                    activeColor: Colors.red.shade300,
+                    value: productPro.isAvailable, 
+                    onChanged: (value) => {
+                      if(!productPro.isAvailable){
+                        productPro.isAvailable = true
+                      } else {
+                        productPro.isAvailable = false
+                      }
+                    }
+                  ),
+                ),
+
+                SizedBox(width: 45),
+
+                Container(
+                  child: _ProductsButton(
+                    code: _codeController,
+                    title: _titleController,
+                    price: _priceController,
+                  ),
+                ),
+              
+              ],
+            ),
+            
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _ProductsButton extends StatelessWidget {
+  
+  final TextEditingController code;
+  final TextEditingController title;
+  final TextEditingController price;
+
+  const _ProductsButton({
+    Key? key, 
+    required this.code, 
+    required this.title, 
+    required this.price, 
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      child: Container(
+        width: 200,
+        height: 50,
+        decoration: BoxDecoration(  
+          color: Colors.red.shade300,
+          borderRadius: BorderRadius.all(Radius.circular(15)),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.5),
+              spreadRadius: 5,
+              blurRadius: 7,
+              offset: Offset(0, 5),
+            )
+          ]
+        ),
+        child: Center(
+          child: Text('Save', style: TextStyle(fontSize: 20, color: Colors.white))
+        )
+      ),
+      // onPressed: () => createAndUpdateAgent(context)
+      onPressed: () => {}
+    );
+  }
+
+  // void _createAgent(BuildContext context, AgentManamegentProvider provider) async {
+
+  //   bool resp;
+  //   String userID = await UserService.readUserID();
+  //   final userProvider = Provider.of<UserProvider>(context, listen: false);
+
+  //   final newAgent = new Agent(
+  //     name           : this.name.text, 
+  //     lastname       : this.lastName.text, 
+  //     email          : this.email.text, 
+  //     phone          : this.phone.text, 
+  //     identification : this.identification.text,
+  //     userID         : userID
+  //   );
+
+  //   resp = await AgentService.createAgent(newAgent, userProvider.token);
+
+  //   if(resp){
+  //     if(provider.isChangePhoto){
+  //       resp = await AgentService.uploadImage(newAgent.identification ?? '', provider.photo, userProvider.token);
+  //     }
+
+  //     if(resp) {
+  //       provider.isChangePhoto = false;
+  //       showAlert(
+  //         context     : context, 
+  //         title       : 'Success', 
+  //         subTitle    : 'Successfully created agent', 
+  //         urlImage    : 'assets/male-icon.jpg', 
+  //         userName    : '${newAgent.name} ${newAgent.lastname}',
+  //         status      : StatusAlert.Success,
+  //         successPage : 'agent',
+  //         cancelPage  : 'agentsOptions'
+  //       );
+  //     } else {
+
+  //       showAlert(
+  //         context  : context, 
+  //         title    : 'Error', 
+  //         subTitle : 'Failed to create an agent', 
+  //         urlImage : 'assets/male-icon.jpg', 
+  //         userName : '${newAgent.name} ${newAgent.lastname}',
+  //         status   : StatusAlert.Error,
+  //         successPage : 'agent',
+  //         cancelPage  : 'agentsOptions'
+  //       );  
+
+  //     }
+  //   } else {
+  //     showAlert(
+  //       context  : context, 
+  //       title    : 'Error', 
+  //       subTitle : 'Failed to create an agent', 
+  //       urlImage : 'assets/male-icon.jpg', 
+  //       userName : '${newAgent.name} ${newAgent.lastname}',
+  //       status   : StatusAlert.Error,
+  //       successPage : 'agent',
+  //       cancelPage  : 'agentsOptions'
+  //     );
+  //   }
+  // }
+
+  // void _updateAgent(BuildContext context, AgentManamegentProvider provider) async {
+    
+  //   bool resp;
+  //   Agent agent = provider.agent;
+  //   String userID = await UserService.readUserID();
+  //   final userProvider = Provider.of<UserProvider>(context, listen: false);
+
+  //   final newAgent = new Agent(
+  //     name           : (this.name.text == '') ? agent.name : this.name.text, 
+  //     lastname       : (this.lastName.text == '') ? agent.lastname : this.lastName.text,
+  //     email          : (this.email.text == '') ? agent.email : this.email.text, 
+  //     phone          : (this.phone.text == '') ? agent.phone : this.phone.text, 
+  //     identification : (this.identification.text == '') ? agent.identification : this.identification.text,
+  //     userID         : userID
+  //   );
+
+  //   resp = await AgentService.updateAgent(newAgent, userProvider.token);
+
+  //   if(resp){
+
+  //     if(provider.isChangePhoto){
+  //       resp = await AgentService.uploadImage(newAgent.identification ?? '', provider.photo, userProvider.token);
+  //     }
+
+  //     if(resp) {
+  //       provider.isChangePhoto = false;
+  //       showAlert(
+  //         context  : context, 
+  //         title    : 'Success', 
+  //         subTitle : 'Successfully updated agent', 
+  //         urlImage : 'assets/male-icon.jpg', 
+  //         userName : '${newAgent.name} ${newAgent.lastname}',
+  //         status   : StatusAlert.Success,
+  //         successPage : 'agent',
+  //         cancelPage  : 'agentsOptions'
+  //       );
+  //     } else {
+  //       showAlert(
+  //         context  : context, 
+  //         title    : 'Error', 
+  //         subTitle : 'Failed to update an agent', 
+  //         urlImage : 'assets/male-icon.jpg', 
+  //         userName : '${newAgent.name} ${newAgent.lastname}',
+  //         status   : StatusAlert.Error,
+  //         successPage : 'agent',
+  //         cancelPage  : 'agentsOptions'
+  //       );  
+  //     }
+  //   } else {
+  //     showAlert(
+  //       context  : context, 
+  //       title    : 'Error', 
+  //       subTitle : 'Failed to update an agent', 
+  //       urlImage : 'assets/male-icon.jpg', 
+  //       userName : '${newAgent.name} ${newAgent.lastname}',
+  //       status   : StatusAlert.Error,
+  //       successPage : 'agent',
+  //       cancelPage  : 'agentsOptions'
+  //     );
+  //   }
+  // }
+
+  // void createAndUpdateAgent(BuildContext context) {
+
+  //   final agentProvider = Provider.of<AgentManamegentProvider>(context, listen: false);
+
+  //   if(!agentProvider.updating) {
+  //     _createAgent(context, agentProvider);
+  //   } else {
+  //     _updateAgent(context, agentProvider);      
+  //   }
+  // }
+
 }
