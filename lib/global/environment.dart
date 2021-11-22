2600caa8498152fdf73c5db6219f2db6d356da7e
Carlos Pereira <carloscoto0103@gmail.com>
2021-11-22 08:02:26 -0600

    environment.dart: Added uploads image products url

--- lib/global/environment.dart
+++ lib/global/environment.dart
@@ -2,15 +2,16 @@
 
 class Environment {
 
-  static String apiAuthLoginUrl    = 'https://rest-server-agents-pereira.herokuapp.com/api/auth/login';
-  static String apiAuthGoogleUrl   = 'https://rest-server-agents-pereira.herokuapp.com/api/auth/google';
-  static String apiAuthValidUrl    = 'https://rest-server-agents-pereira.herokuapp.com/api/auth/validJWT';
+  static String apiAuthLoginUrl        = 'https://rest-server-agents-pereira.herokuapp.com/api/auth/login';
+  static String apiAuthGoogleUrl       = 'https://rest-server-agents-pereira.herokuapp.com/api/auth/google';
+  static String apiAuthValidUrl        = 'https://rest-server-agents-pereira.herokuapp.com/api/auth/validJWT';
 
-  static String apiAgentsUrl       = 'https://rest-server-agents-pereira.herokuapp.com/api/agents';
-  static String apiUserUrl         = 'https://rest-server-agents-pereira.herokuapp.com/api/users';
-  static String apiProductsUrl     = 'https://rest-server-agents-pereira.herokuapp.com/api/products';
+  static String apiAgentsUrl           = 'https://rest-server-agents-pereira.herokuapp.com/api/agents';
+  static String apiUserUrl             = 'https://rest-server-agents-pereira.herokuapp.com/api/users';
+  static String apiProductsUrl         = 'https://rest-server-agents-pereira.herokuapp.com/api/products';
 
-  static String apiUploadAgentsUrl = 'https://rest-server-agents-pereira.herokuapp.com/api/uploads/agents';
-  static String apiUploadUsersUrl  = 'https://rest-server-agents-pereira.herokuapp.com/api/uploads/users';
+  static String apiUploadAgentsUrl     = 'https://rest-server-agents-pereira.herokuapp.com/api/uploads/agents';
+  static String apiUploadUsersUrl      = 'https://rest-server-agents-pereira.herokuapp.com/api/uploads/users';
+  static String apiUploadProductsUrl  = 'https://rest-server-agents-pereira.herokuapp.com/api/uploads/products';
 
 }
